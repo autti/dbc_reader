f12e2f8390f878129c2f9e28bce15a60f19203ed
Jefferson Javier Hernández Panqueba <jeffersonh44@gmail.com>
2018-07-09 11:51:05 -0500

    can logs added

---
+++ bash/start-can.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo modprobe can
+sudo modprobe can-raw
+sudo modprobe can-dev
+sudo modprobe vcan
+sudo modprobe kvaser_usb
+
+sudo ip link add dev hs1 type vcan
+sudo ip link add dev hs2 type vcan
+sudo ip link add dev hs3 type vcan
+sudo ip link add dev ms1 type vcan
+
+sudo ip link set up hs1
+sudo ip link set up hs2
+sudo ip link set up hs3
+sudo ip link set up ms1
+
+/usr/bin/tmux new-session -s fakecan -d canplayer -l i -I ./80can.log hs1=can0 hs2=can1 hs3=can2
